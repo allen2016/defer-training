25fde0ec711ea8dfb4a819ddb0fac09d9f5c8149
allen2016 <allen200000@126.com>
2014-09-10 21:20:21 +0800

    Signed-off-by: allen2016 <allen200000@126.com>

--- DeferTrain/src/com/neusoft/activities/HomeAty.java
+++ DeferTrain/src/com/neusoft/activities/HomeAty.java
@@ -1,9 +1,25 @@
 package com.neusoft.activities;
 
+import com.mycustom.view.TitleView;
+import com.neusoft.defertrain.R;
+
 import android.app.Activity;
+import android.os.Bundle;
 
 public class HomeAty extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.example_layout);
+		
+		TitleView titleView = (TitleView) findViewById(R.id.mytitleview);
+		titleView.setTitleText("这是标题栏");
+		titleView.setBackgroundColor(getResources().getColor(R.color.vpi__background_holo_dark));
+	}
 
+	
 	
 }
